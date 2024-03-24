0f3762484a34d1f2aa5a17c66c8a5df23239b9ae
Wendel <wendelsouza.dev@gmail.com>
2024-03-24 14:04:04 -0300

    Controller de aeroportos

---
+++ app/Http/Controllers/AeroportoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aeroporto;
+use Illuminate\Http\Request;
+
+class AeroportoController extends Controller
+{
+    public function buscaAeroporto($id = null)
+    {
+        if($id !== null){
+
+            $aeroporto = Aeroporto::find($id);
+
+            return response()->json($aeroporto);
+        }
+        $aeroportos = Aeroporto::all();
+        // var_dump($aeroportos);exit;
+        return response()->json($aeroportos);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AeroportoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aeroportos/{id?}', [AeroportoController::class, 'buscaAeroporto']);
